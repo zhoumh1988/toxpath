27d0eaaeb3a5046c8318e62574e28441809bfa58
zhouminghua <zhoumh1988@163.com>
2019-09-12 14:40:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# toxpath
+
+## 介绍
+
+将 HTML Element 转换成 xpath 表达式。
+
+## 使用
+
+安装依赖
+
+```ssh
+npm i toxpath --save
+```
+
+demo
+
+```javascript
+const toxpath = require("toxpath");
+const xpathStr = toxpath(dom);
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * 判断对象是否为空或者空串
+ * @param {Object} o 
+ */
+function isEmpty(o) {
+    return null === o || 'null' === o || '' === o || undefined === o;
+}
+
+/**
+ * 获取节点的位置属性
+ * @param {HTMLElement} e 
+ */
+function getAttr(e) {
+    var tn = e.tagName;
+    var id = e.getAttribute('id');
+    var hasId = !isEmpty(id);
+    id = "[@id='" + id + "']";
+    if (hasId) {
+        return id;
+    } else {
+        let preElement = e.previousElementSibling;
+        let i = 1;
+        while (!!preElement) {
+            if (preElement.tagName === tn) {
+                i++;
+            }
+            preElement = preElement.previousElementSibling;
+        }
+        return '[' + i + ']';
+    }
+}
+
+function getPath(elementDom, path = '') {
+    var tn = elementDom.tagName;
+    if (isEmpty(elementDom) || isEmpty(tn)) {
+        return path;
+    }
+    var attr = getAttr(elementDom);
+    tn = tn.toLowerCase() + attr;
+    path = isEmpty(path) ? tn : tn + "/" + path;
+    var parentE = elementDom.parentElement;
+    if (isEmpty(parentE) || attr.substring(0, 5) == '[@id=') {
+        return path;
+    }
+    return getPath(parentE, path);
+}
+
+/**
+ * 获取节点的xpath表达式
+ * @param {HTMLElement} dom 
+ */
+module.exports = function (dom) {
+    return `//${getPath(dom)}}`
+}
